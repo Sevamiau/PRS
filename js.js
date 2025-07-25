187dad35927e99e8ee5cc6f9157fd4cf43a884bc
¨Seva <sevakunjadas.bms@gmail.com>
2025-07-25 01:42:02 -0300

    falta score function

--- js.js
+++ js.js
@@ -1,106 +1,53 @@
-
-
-let playerScore = 0;
+let humanScore = 0;
 let computerScore = 0;
 
+const weapons = ['rock', 'paper', 'scissors'];
 
-function playRound() {
-
-
-    // PLAYERS CHOICE
-
-    let playerChoice = null;
-
-    let rawInput = prompt('elegi piedra papel o tijera: ')
-     processedInput = rawInput.toLowerCase();
-
-    if (processedInput === 'piedra' || processedInput === 'papel' || processedInput === 'tijera'){
-        playerChoice = processedInput;
-    } else {
-        alert('deja de tirar fruta siome')
-    }
-
-    console.log('vos elegiste: ', playerChoice)
-
-    // COMPUTADORA CHOICE
-
-    let computerNumber = Math.floor(Math.random() * 3);
-    let computerChoice;
-
-    if (computerNumber === 0) {
-        computerChoice = 'piedra';
-    } else if (computerNumber === 1) {
-        computerChoice = 'papel';
-    } else {
-        computerChoice = 'tijera';
-    }
-
-    console.log('La maquina eligio: ', computerChoice)
-
-    // COMPUTADORA WINS
-
-    if (computerChoice === 'piedra' && playerChoice === 'tijera'){
-        console.log('Perdiste!');
-        return 'computer';
-    } else if (computerChoice === 'papel' && playerChoice === 'piedra'){
-        console.log('Perdiste!'); 
-        return 'computer'  
-    } else if (computerChoice === 'tijera' && playerChoice === 'papel'){
-        console.log('Perdiste!')
-        return 'computer'
-    }
-
-    // PLAYER WINS
-
-    if (playerChoice === 'piedra' && computerChoice === 'tijera') {
-        console.log('GANASTE!');
-        return 'player'
-    } else if (playerChoice === 'papel' && computerChoice === 'piedra'){
-        console.log('GANASTE!');
-        return 'player'
-    } else if(playerChoice === 'tijera' && computerChoice === 'papel'){
-        console.log('GANASTE!');
-        return 'player'
-    }
-
-    // TIE
-
-    if (playerChoice === computerChoice){
-        console.log('EMPATE!');
-        return 'tie'
-    }
-
-    }
-
-// playRound()
-
-
-while (playerScore < 5 && computerScore < 5){
-    const roundWinner = playRound();
-    if (roundWinner === 'player'){
-        playerScore += 1;
-    } else if (roundWinner === 'computer'){
-        computerScore +=1;
-    }
-
-    console.log(`Puntuacion: jugador ${playerScore} - computadora ${computerScore} `)
-
+// COMPUTERS CHOICE
+function getComputerChoice() {
+    const cChoice = weapons;
+    const cInput = cChoice[Math.floor(Math.random() * cChoice.length)];
+    return cInput;
 }
 
+// HUMANS CHOICE
+function getHumanChoice() {
+    // Prompt the user for their choice
+    let hChoice = prompt('Choose: rock, paper, or scissors');
 
+    // Keep prompting until a valid choice is entered
+    while (!weapons.includes(hChoice.toLowerCase())) {
+        hChoice = prompt('Invalid choice. Please choose: rock, paper, or scissors');
+    }
 
-console.log("--------------------");
-console.log("¡JUEGO TERMINADO!");
-
-if (playerScore === 5) {
-    console.log("LE GANASTE A LA MAQUINA!");
-} else {
-    console.log("TE GANO LA MAQUINA MANCO ");
+    const hInput = hChoice.toLowerCase();
+    return hInput;
 }
 
+function playRound(humanSelection, computerSelection) {
+    console.log(`You chose: ${humanSelection}`);
+    console.log(`The computer chose: ${computerSelection}`);
 
+    if (humanSelection === computerSelection) {
+        console.log("It's a tie!");
+    }
+    else if (
+        (humanSelection === 'rock' && computerSelection === 'scissors') ||
+        (humanSelection === 'scissors' && computerSelection === 'paper') ||
+        (humanSelection === 'paper' && computerSelection === 'rock')
+    ) {
+        humanScore++;
+        console.log(`You win this round! ${humanSelection} beats ${computerSelection}.`);
+    }
+    else {
+        computerScore++;
+        console.log(`You lose this round! ${computerSelection} beats ${humanSelection}.`);
+    }
 
+    console.log(`Score -> You: ${humanScore} | Computer: ${computerScore}`);
+}
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-
-
+playRound(humanSelection, computerSelection);
